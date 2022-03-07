e32229c3bdc701b0e6375447a8a12faaa995632e
tse018 <60438717+tse018@users.noreply.github.com>
2022-03-07 10:26:06 +0100

    Setup mapbox-gl

---
+++ project/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+<div id='map'></div>
+
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+
+export default {
+   data() {
+      return {
+         mapbox_id: import.meta.env.VITE_MAPBOX_ID,
+      };
+   },
+
+   mounted() {
+      mapboxgl.accessToken = this.mapbox_id;
+      const map = new mapboxgl.Map({
+         container: "map", // container ID
+         style: "mapbox://styles/mapbox/streets-v11", // style URL
+         center: [52.519325, 13.392709], // starting position [lng, lat]
+         zoom: 1, // starting zoom
+      });
+   },
+};
+</script>
+
+<style scoped>
+#map {
+   width: 100vw; 
+   height: 100vh;
+   margin: 0px;
+   padding: 0px
+}
+</style>
